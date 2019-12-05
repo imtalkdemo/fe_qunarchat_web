5b129a630fea26769c794a4af0e37e99f481ab51
zhangchao11 <zhangchao11@360.cn>
2019-12-05 12:03:08 +0800

    add qtalk nav

--- src/navigation.js
+++ src/navigation.js
@@ -1,5 +1,18 @@
 window.nav_igator = {
     "baseaddess": {
-
+        "domain":"qtalk",
+        "fileurl":"https://qtalk:8080",
+        "protobufPcPort":5202,
+        "socketurl":"wss://qtalk:5280/websocket",
+        "pubkey":"rsa_public_key",
+        "xmppmport":5222,
+        "xmppport":5222,
+        "domainhost":"qtalk",
+        "simpleapiurl":"https://qtalk:8080",
+        "payurl":"https://qtalk:8080/redpackage",
+        "apiurl":"https://qtalk:8080/api",
+        "javaurl":"https://qtalk:8080/package",
+        "xmpp":"qtalk:5280",
+        "protobufPort":5202
       }
   };
